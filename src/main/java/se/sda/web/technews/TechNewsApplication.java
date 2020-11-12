c55d0ed3d3eb0f875ec79fdaf317ee8909e09dcf
awesome-button <oleshko.lena@gmail.com>
2020-11-12 15:51:32 +0100

    Setup with Spring Boot

---
+++ src/main/java/se/sda/web/technews/TechNewsApplication.java
@@ -0,0 +1,13 @@
+package se.sda.web.technews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/sda/web/technews/TechNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sda.web.technews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
